492d51dafddb56f5895bc8f356ac7258656f7e10
kamilhayat <119631915+kamilhayat@users.noreply.github.com>
2023-10-23 20:52:16 +0530

    Delete routes directory

--- routes/booking-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const bookingRouter = express.Router();
-
-const { newBooking, deleteBooking } = require('../controllers/booking-controller');
-
-bookingRouter.post("/", newBooking);
-bookingRouter.delete('/:id', deleteBooking);
- 
-module.exports = bookingRouter; 
